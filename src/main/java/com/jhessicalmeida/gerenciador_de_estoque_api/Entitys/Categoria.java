6cb500ebdc97952cd6edccfa3527ac713fafd7a0
Jheyjaygq <ja33278@gmail.com>
2025-05-06 20:54:39 -0300

    Criação das entidades categoria e produto

--- src/main/java/com/jhessicalmeida/gerenciador_de_estoque_api/Entitys/Categoria.java
+++ src/main/java/com/jhessicalmeida/gerenciador_de_estoque_api/Entitys/Categoria.java
@@ -1,5 +1,54 @@
 package com.jhessicalmeida.gerenciador_de_estoque_api.Entitys;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Table(name = "categoria")
+@Entity
 public class Categoria {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+     @Column(nullable = false, unique = true)
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+
+    @OneToMany(mappedBy = "categoria")
+    @JsonIgnore
+    private List<Produto> produtos;
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+
 }
--- src/main/java/com/jhessicalmeida/gerenciador_de_estoque_api/Entitys/Produto.java
+++ src/main/java/com/jhessicalmeida/gerenciador_de_estoque_api/Entitys/Produto.java
@@ -1,5 +1,85 @@
 package com.jhessicalmeida.gerenciador_de_estoque_api.Entitys;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+    
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name = "produto")
+@Entity
 public class Produto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double preco;
+
+
+    private String imagem; 
+
+    
+    @Column(nullable = false)
+    private int quantidade;
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
 }
